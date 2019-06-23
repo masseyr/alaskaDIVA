d196ed68cfb67f0871c988894aa7eb3e9017252f
Richard Massey <rm885@nau.edu>
2019-06-23 03:14:05 -0700

    updated imports

--- modules/__init__.py
+++ modules/__init__.py
@@ -2,6 +2,6 @@ from common import Sublist, Handler, FTPHandler, Opt, \
     OGR_GEOM_DEF, OGR_TYPE_DEF, OGR_FIELD_DEF, GDAL_FIELD_DEF, OGR_FIELD_DEF_INV, GDAL_FIELD_DEF_INV
 from vector import Vector
 from logger import Logger
-from distance import Mahalanobis, Distance
+from distance import Distance, Euclidean
 from samples import Samples
 from timer import Timer
